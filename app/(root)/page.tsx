211d3f01b696db263b01ddbf986bc56670401938
tzafff <christzaf99@gmail.com>
2024-11-22 11:33:41 +0200

    Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,24 @@
+import SearchForm from "@/components/SearchForm";
 
 
-export default function Home() {
-  return (
-    <>
-        <section className={"pink_container"}>
-            <h1 className={"heading"}>Pitch Your Startup, <br/> Connect With
-                Entrepreneurs
-            </h1>
+export default async function Home({searchParams}: { searchParams: Promise<{ query?: string }> }) {
+    const query  = (await searchParams).query
 
-            <p className={"sub-heading !max-w-3xl"}>
-                Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
-            </p>
+    return (
+        <>
+            <section className={"pink_container"}>
+                <h1 className={"heading"}>Pitch Your Startup, <br/> Connect With
+                    Entrepreneurs
+                </h1>
+
+                <p className={"sub-heading !max-w-3xl"}>
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+                </p>
+                <SearchForm query={query}/>
+
+            </section>
 
 
-        </section>
-
-
-    </>
-  );
+        </>
+    );
 }
