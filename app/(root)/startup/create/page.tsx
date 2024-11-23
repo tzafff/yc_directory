2a2695fc2921e46b2987ff5f2665e6f2ed05069d
tzafff <christzaf99@gmail.com>
2024-11-23 18:43:25 +0200

    Create Startup Form

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import StartupForm from "@/components/StartupForm";
+import {auth} from "@/auth";
+import {redirect} from "next/navigation";
+
+const Page = async () => {
+    const session = await auth();
+
+    if(!session) redirect('/');
+
+    return (
+        <>
+            <section className={"pink_container !min-h-[210px]"}>
+                <h1 className={"heading"}>Submit Your Startup</h1>
+            </section>
+
+            <StartupForm />
+        </>
+    )
+}
+export default Page
